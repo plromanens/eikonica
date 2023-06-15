fca23bffb5f9a5a84b60d7174ca9a5d16c1bf121
plromanens <Pierre-Louis.Romanens@studentfr.ch>
2023-06-15 17:04:01 +0200

    commit du jour

--- site_ppro2023/src/js/main.js
+++ site_ppro2023/src/js/main.js
@@ -4,3 +4,16 @@ const menu = document.querySelector(".links");
 burgerIcon.addEventListener("click", () => {
   menu.classList.toggle("show");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var imageContainer = document.querySelector(".image-container");
+  var hoverImage = document.querySelector(".hover-image");
+
+  imageContainer.addEventListener("click", function () {
+    if (hoverImage.style.display === "none") {
+      hoverImage.style.display = "block";
+    } else {
+      hoverImage.style.display = "none";
+    }
+  });
+});
